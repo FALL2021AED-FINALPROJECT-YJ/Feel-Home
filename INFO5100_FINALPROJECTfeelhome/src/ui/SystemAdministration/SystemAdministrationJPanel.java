32ded32a056da6236d7e0682c2bcd2b74a02d06c
Jaahnavikandula <91014931+Jaahnavikandula@users.noreply.github.com>
2021-12-02 22:21:27 -0800

    updated panel

--- INFO5100_FINALPROJECTfeelhome/src/ui/SystemAdministration/SystemAdministrationJPanel.java
+++ INFO5100_FINALPROJECTfeelhome/src/ui/SystemAdministration/SystemAdministrationJPanel.java
@@ -1,102 +1,127 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
+
 package ui.SystemAdministration;
 
 import model.SystemAdmin;
 
-/**
- *
- * @author jaahn
- */
 public class SystemAdministrationJPanel extends javax.swing.JPanel {
-
-   SystemAdmin systems;
-  private Runnable callOnCreateMethod;
-
-    public SystemAdministrationJPanel(SystemAdmin systems, Runnable callOnCreateMethod) {
+  SystemAdmin systems;
+  private Runnable callOnCreateMethod1;
+  private Runnable callOnCreateMethod2;
+  private Runnable callOnCreateMethod3;
+  private Runnable callOnCreateMethod4;
+    public SystemAdministrationJPanel(SystemAdmin systems, Runnable callOnCreateMethod1,Runnable callOnCreateMethod2, Runnable callOnCreateMethod3,Runnable callOnCreateMethod4) {
         initComponents();
         this.systems = systems;
-        this.callOnCreateMethod = callOnCreateMethod;
+        this.callOnCreateMethod1 = callOnCreateMethod1;
+       this.callOnCreateMethod2 = callOnCreateMethod2;
+       this.callOnCreateMethod3 = callOnCreateMethod3;
+       this.callOnCreateMethod4 = callOnCreateMethod4;
     }
+
+  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         btnmanagenetworks = new javax.swing.JButton();
-        btnmanageenterprise = new javax.swing.JButton();
+        enterpriseBtn = new javax.swing.JButton();
         btnmanagecustomers = new javax.swing.JButton();
         lblsysadmin = new javax.swing.JLabel();
         btnmanagemanagers = new javax.swing.JButton();
 
-        setBackground(new java.awt.Color(255, 153, 153));
-
         btnmanagenetworks.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         btnmanagenetworks.setText("MANAGE NETWORKS");
-
-        btnmanageenterprise.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        btnmanageenterprise.setText("MANAGE ENTERPRICES");
-        btnmanageenterprise.addActionListener(new java.awt.event.ActionListener() {
+        btnmanagenetworks.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnmanageenterpriseActionPerformed(evt);
+                btnmanagenetworksActionPerformed(evt);
+            }
+        });
+
+        enterpriseBtn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        enterpriseBtn.setText("MANAGE ENTERPRICES");
+        enterpriseBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                enterpriseBtnActionPerformed(evt);
             }
         });
 
         btnmanagecustomers.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         btnmanagecustomers.setText("MANAGE CUSTOMERS");
+        btnmanagecustomers.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnmanagecustomersActionPerformed(evt);
+            }
+        });
 
         lblsysadmin.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         lblsysadmin.setText("SYSTEM ADMINISTRATOR");
 
         btnmanagemanagers.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         btnmanagemanagers.setText("MANAGE MANAGERS");
+        btnmanagemanagers.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnmanagemanagersActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(478, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(297, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblsysadmin, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblsysadmin)
+                        .addGap(272, 272, 272))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(btnmanageenterprise, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(enterpriseBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnmanagenetworks, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnmanagecustomers, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnmanagemanagers, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(163, 163, 163)))
-                .addGap(377, 377, 377))
+                            .addComponent(btnmanagemanagers, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(435, 435, 435))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(154, 154, 154)
+                .addGap(97, 97, 97)
                 .addComponent(lblsysadmin)
                 .addGap(69, 69, 69)
                 .addComponent(btnmanagenetworks)
                 .addGap(58, 58, 58)
-                .addComponent(btnmanageenterprise)
+                .addComponent(enterpriseBtn)
                 .addGap(66, 66, 66)
                 .addComponent(btnmanagemanagers)
                 .addGap(73, 73, 73)
                 .addComponent(btnmanagecustomers)
-                .addContainerGap(225, Short.MAX_VALUE))
+                .addContainerGap(249, Short.MAX_VALUE))
         );
     }// </editor-fold>                        
 
-    private void btnmanageenterpriseActionPerformed(java.awt.event.ActionEvent evt) {                                                    
-        // TODO add your handling code here:
-         callOnCreateMethod.run();
-    }                                                   
+    private void enterpriseBtnActionPerformed(java.awt.event.ActionEvent evt) {                                              
+      callOnCreateMethod2.run();
+      
+    }                                             
+
+    private void btnmanagenetworksActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+        callOnCreateMethod1.run();
+    }                                                 
+
+    private void btnmanagecustomersActionPerformed(java.awt.event.ActionEvent evt) {                                                   
+        callOnCreateMethod4.run();
+    }                                                  
+
+    private void btnmanagemanagersActionPerformed(java.awt.event.ActionEvent evt) {                                                  
+          callOnCreateMethod3.run();
+    }                                                 
 
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton btnmanagecustomers;
-    private javax.swing.JButton btnmanageenterprise;
     private javax.swing.JButton btnmanagemanagers;
     private javax.swing.JButton btnmanagenetworks;
+    private javax.swing.JButton enterpriseBtn;
     private javax.swing.JLabel lblsysadmin;
     // End of variables declaration                   
 }
