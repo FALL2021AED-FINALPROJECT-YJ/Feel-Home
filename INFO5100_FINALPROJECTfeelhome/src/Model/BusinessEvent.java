617f2d482d89e8f7e76b2f14f939dc34bebb1b94
Jaahnavikandula <91014931+Jaahnavikandula@users.noreply.github.com>
2021-12-04 19:05:23 -0800

    added few other things

--- INFO5100_FINALPROJECTfeelhome/src/Model/BusinessEvent.java
+++ INFO5100_FINALPROJECTfeelhome/src/Model/BusinessEvent.java
@@ -4,17 +4,20 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BusinessEvent {
-     private String username;
-    private String password;
-    private List<Manager> listOfManager;
-    private OraganisationdirectoryForEvents orgDirecForEvents;
+public class BusinessEvent extends Enterprise {
 
-    public BusinessEvent(String name, String contact, String username, String password) {
+    private List<Manager> listOfManager;
+    private List<CateringService> listOfCatering;
+    private List<PhotographyService> listOfPhotographyServices;
+    private List<DecorService> listOfDecors;
+
+    public BusinessEvent(String name, String contact) {
         super(name, contact);
-        this.username = username;
-        this.password = password;
         listOfManager = new ArrayList<>();
+        listOfCatering = new ArrayList<>();
+        listOfPhotographyServices = new ArrayList<>();
+        listOfDecors = new ArrayList<>();
+
     }
 
     public List<Manager> getListOfManager() {
@@ -25,7 +28,62 @@ public class BusinessEvent {
         this.listOfManager = listOfManager;
     }
 
-    public OraganisationdirectoryForEvents getOrgDirecForEvents() {
-        return orgDirecForEvents;
+    public List<CateringService> getListOfCatering() {
+        return listOfCatering;
     }
+
+    public void setListOfCatering(List<CateringService> listOfCatering) {
+        this.listOfCatering = listOfCatering;
+    }
+
+    public List<PhotographyService> getListOfPhotographyServices() {
+        return listOfPhotographyServices;
+    }
+
+    public void setListOfPhotographyServices(List<PhotographyService> listOfPhotographyServices) {
+        this.listOfPhotographyServices = listOfPhotographyServices;
+    }
+
+    public List<DecorService> getListOfDecors() {
+        return listOfDecors;
+    }
+
+    public void setListOfDecors(List<DecorService> listOfDecors) {
+        this.listOfDecors = listOfDecors;
+    }
+
+    public Manager addManager(String name, String username, String password) {
+        Manager manager = new Manager(name, username, password);
+        listOfManager.add(manager);
+        return manager;
+    }
+
+    public Manager findManager(String username) {
+        for (Manager man : listOfManager) {
+            if (man.getUserName().equals(username)) {
+                return man;
+            }
+        }
+        return null;
+    }
+
+    public void addCateringService(String name, String contact, String city) {
+        CateringService cat = new CateringService(name, contact, city);
+        System.out.println(name);
+        listOfCatering.add(cat);
+            System.out.println("catering size is " +listOfCatering.size()+ " name is " +listOfCatering.get(0).getName());
+
+    }
+
+    public void addPhotographyService(String name, String contact, String city) {
+        PhotographyService photo = new PhotographyService(name, contact, city);
+        listOfPhotographyServices.add(photo);
+
+    }
+
+    public void addDecorService(String name, String contact, String city) {
+        DecorService decor = new DecorService(name, contact, city);
+        listOfDecors.add(decor);
+    }
+
 }
